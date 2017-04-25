fcb5368750b45264600c41c07860da126707cbaf
jenksy <jjenksy@gmail.com>
2017-04-25 12:40:23 -0500

    added application.yml and added a PersonProperties class to access custom configuration

--- src/main/java/com/jjenksydemo/SpringDemoJjenksyApplication.java
+++ src/main/java/com/jjenksydemo/SpringDemoJjenksyApplication.java
@@ -1,12 +1,23 @@
 package com.jjenksydemo;
 
+import com.jjenksydemo.ctrl.PersonProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringDemoJjenksyApplication {
 
+	final private PersonProperties personProperties;
+
+	@Autowired
+	public SpringDemoJjenksyApplication(PersonProperties personProperties){
+		this.personProperties = personProperties;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoJjenksyApplication.class, args);
 	}
+
+
 }
