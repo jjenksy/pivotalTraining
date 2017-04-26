0ef30bb2079f4f704d668cb1bf12b8ac48d08a24
jenksy <jjenksy@gmail.com>
2017-04-26 12:26:25 -0500

    added config properties to class path

--- src/main/java/com/jjenksydemo/SpringDemoJjenksyApplication.java
+++ src/main/java/com/jjenksydemo/SpringDemoJjenksyApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDemoJjenksyApplication {
 
+
 	final private PersonProperties personProperties;
 
 	@Autowired
